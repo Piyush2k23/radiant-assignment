6ad3f21ba73d81292d9d4baf2ad63ffb8573f889
Piyush2k23 <piyush23pr2000@gmail.com>
2024-02-25 23:29:41 +0530

    Creating main card component

--- src/components/main-card/MainCard.jsx
+++ src/components/main-card/MainCard.jsx
@@ -1,10 +1,16 @@
 import "./mainCard.scss";
 import cardImage from "../../assets/card-image.png";
 import { IoIosArrowDown } from "react-icons/io";
+import { SlTrophy } from "react-icons/sl";
 
 const MainCard = () => {
   return (
+    <>
+
     <div className="main-container">
+    <div className="best-container"><SlTrophy className="trophy-icon"/><span>Best Choice</span></div>
+    <div className="number-container"><span>1</span></div>
+    
         <div className="left-container">
             <img src={cardImage} alt="card-image" />
             <span>Builder 1</span>
@@ -13,8 +19,8 @@ const MainCard = () => {
             <p><span>WixPro 72-Inch Responsive Website Builder- </span>Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)
            </p>
            <h5>Main highlights</h5>
-           <p>[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
-            <div className="show"><span>Show More</span><IoIosArrowDown /></div>
+           <p className="second-parg">[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
+            <div className="show"><span>Show more</span><IoIosArrowDown className="down-arrow"/></div>
         </div>
         <div className="right-container">
             <div className="rating-container">
@@ -25,6 +31,7 @@ const MainCard = () => {
             <button className="btn">View</button>
         </div>
     </div>
+    </>
   )
 }
 
